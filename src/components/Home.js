b1691f145c371d448c439db6366f2ab8796f6bff
jfvandermousen <jeanfrancoisvandermousen@gmail.com>
2021-08-06 09:43:13 +0200

    push

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     return  (
         <div className="">
             <header className="mb-10">
-            <div className="logo text-3xl"><h1>IMMO ELISA</h1></div>
+            <div className="logo text-3xl"><h1>IMMOELIZA</h1></div>
             <nav className="flex ">
 
                 <div className="nav" >HOME</div>
