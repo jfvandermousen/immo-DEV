0b4ac4c04e6d5546db7a6fc0d61371c20565bae0
jfvandermousen <jeanfrancoisvandermousen@gmail.com>
2021-09-21 10:38:06 +0200

    change width header wrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,12 +7,13 @@ const Home = () => {
     return  (
         <div className="">
             <header className="mb-10">
+                <div className="wrap">
             <div className="logo text-3xl"><h1>IMMOELIZA</h1></div>
             <nav className="flex ">
-
-                <div className="nav" >HOME</div>
+                <div className="nav-home" >HOME</div>
                 <div className="nav" >CONTACT</div>
             </nav>
+            </div>
             </header>
         <div className=' title container flex flex-col items-start justify-center '>
             <h2 className="text-3xl"> Find the price of your new property !</h2>
